427ab4fd9615dd231a427f7da0c573c70ac45c6b
Mike Ferrara <mferrara@gmail.com>
2018-03-01 01:28:47 -0800

    Give seeded users some points for testing link-serving

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -36,7 +36,7 @@ class UsersTableSeeder extends Seeder
             $user->forceFill([
                 'name'                          => $faker->name,
                 'email'                         => $faker->email,
-                'points'                        => 0,
+                'points'                        => 1000,
                 'password'                      => bcrypt('password'),
                 'last_read_announcements_at'    => Carbon::now(),
                 'trial_ends_at'                 => Carbon::now()->addDays(Spark::trialDays()),
