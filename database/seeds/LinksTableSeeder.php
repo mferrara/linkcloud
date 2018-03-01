95beaca50c133cfe0dac03fd2cad7cb5fa5ca5eb
Mike Ferrara <mferrara@gmail.com>
2018-03-01 00:59:26 -0800

    Diversify and increase number of seeded links per user

--- database/seeds/LinksTableSeeder.php
+++ database/seeds/LinksTableSeeder.php
@@ -11,39 +11,28 @@ class LinksTableSeeder extends Seeder
      */
     public function run()
     {
-        $links = [
-            'https://domain.com/link1,anchor1,3',
-            'https://domain.com/link2,anchor1,4',
-            'https://domain.com/link3,anchor1,5',
-            'https://domain.com/link4,anchor2,6',
-            'https://domain.com/link5,anchor2,2',
-            'https://domain7.com/link6,anchor2,1',
-            'https://domain7.com/link7,anchor2,3',
-            'https://domain7.com/link8,anchor3,6',
-            'https://domain7.com/link9,anchor3,4',
-            'https://domain6.com/link1,anchor3,8',
-            'https://domain6.com/link2,anchor3,7',
-            'https://domain1.com/link3,anchor3,5',
-            'https://domain4.com/link4,anchor4,3',
-            'https://domain1.com/link5,anchor4,2',
-            'https://domain1.com/link6,anchor5,7',
-            'https://domain4.com/link7,anchor5,9',
-            'https://domain1.com/link8,anchor5,4',
-            'https://domain1.com/link9,anchor5,3',
-            'https://domain4.com/link1,anchor6,5',
-            'https://domain2.com/link2,anchor6,7',
-            'https://domain2.com/link3,anchor7,2',
-            'https://domain3.com/link4,anchor7,4',
-            'https://domain5.com/link5,anchor7,3',
-            'https://domain5.com/link6,anchor8,5',
-            'https://domain5.com/link7,anchor8,1',
-            'https://domain2.com/link8,anchor8,3',
-            'https://domain2.com/link9,anchor9,6',
-        ];
+        $links = [];
+
+        // Max links added to the link pool
+        $max        = 10000;
+        $extentions = ['.com', '.net', '.info', '.org', '.xyz', '.tk'];
+        $paths      = ['link', 'page', 'user', 'view'];
+        $anchors    = ['red', 'blue', 'green', 'yellow', 'orange', 'black', 'brown'];
+        while(count($links) < $max)
+        {
+            // Build a random link
+            $link = 'https://domain'.rand(0,9).$extentions[array_rand($extentions)].'/'.$paths[array_rand($paths)].rand(0,9).','.$anchors[array_rand($anchors)].rand(0,9).','.rand(1,15);
+            // If it doesn't already exit, add it
+            if( ! in_array($link, $links))
+                $links[] = $link;
+        }
+
+        // Create a collection
+        $links = collect($links);
 
         foreach(\App\User::all() as $user)
         {
-            $user->importLinks($links);
+            $user->importLinks($links->take(rand(25,500)));
         }
     }
 }
