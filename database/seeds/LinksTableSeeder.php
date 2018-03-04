513741d1ee77d142ad85087ce898425bfc87f0a5
Mike Ferrara <mferrara@gmail.com>
2018-03-04 00:37:21 -0800

    Many more seeded links on production

--- database/seeds/LinksTableSeeder.php
+++ database/seeds/LinksTableSeeder.php
@@ -15,13 +15,15 @@ class LinksTableSeeder extends Seeder
 
         // Max links added to the link pool
         $max        = 10000;
+        if(App::environment() == 'production')
+            $max    = 1000000;
         $extentions = ['.com', '.net', '.info', '.org', '.xyz', '.tk'];
         $paths      = ['link', 'page', 'user', 'view'];
         $anchors    = ['red', 'blue', 'green', 'yellow', 'orange', 'black', 'brown'];
         while(count($links) < $max)
         {
             // Build a random link
-            $link = 'https://domain'.rand(0,9).$extentions[array_rand($extentions)].'/'.$paths[array_rand($paths)].rand(0,9).','.$anchors[array_rand($anchors)].rand(0,9).','.rand(1,15);
+            $link = 'https://domain'.rand(0,99).$extentions[array_rand($extentions)].'/'.$paths[array_rand($paths)].rand(0,99).','.$anchors[array_rand($anchors)].rand(0,99).','.rand(1,15);
             // If it doesn't already exit, add it
             if( ! in_array($link, $links))
                 $links[] = $link;
@@ -30,9 +32,14 @@ class LinksTableSeeder extends Seeder
         // Create a collection
         $links = collect($links);
 
+        echo PHP_EOL;
         foreach(\App\User::all() as $user)
         {
-            $user->importLinks($links->take(rand(25,500)));
+            $insert_count = rand(250,1000);
+            if(App::environment() == 'production')
+                $insert_count = rand(2500,50000);
+            $user->importLinks($links->take($insert_count));
+            echo $user->id.' imported '.number_format($user->links()->count()).PHP_EOL;
         }
     }
 }
